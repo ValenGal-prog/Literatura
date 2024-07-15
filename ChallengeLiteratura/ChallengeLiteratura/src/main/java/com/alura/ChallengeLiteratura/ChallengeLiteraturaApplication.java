19dec94db9446726597d14136abb97c19bebebba
ValenGal-prog <lesliegaleano637@gmail.com>
2024-07-14 21:17:55 -0500

    Update ChallengeLiteraturaApplication.java

--- ChallengeLiteratura/ChallengeLiteratura/src/main/java/com/alura/ChallengeLiteratura/ChallengeLiteraturaApplication.java
+++ ChallengeLiteratura/ChallengeLiteratura/src/main/java/com/alura/ChallengeLiteratura/ChallengeLiteraturaApplication.java
@@ -6,8 +6,19 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class ChallengeLiteraturaApplication {
 
+	@Autowired
+	private AutorRepository autorRepository;
+
+	@Autowired
+	private LibroRepository libroRepository;
+
 	public static void main(String[] args) {
-		SpringApplication.run(ChallengeLiteraturaApplication.class, args);
+		SpringApplication.run(LiterAluraMbApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+
+		Principal principal = new Principal(libroRepository, autorRepository);
+		principal.mostrarMenuOpciones();
 }
