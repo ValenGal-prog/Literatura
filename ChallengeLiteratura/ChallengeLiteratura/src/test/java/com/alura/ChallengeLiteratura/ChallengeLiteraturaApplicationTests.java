c7774784255a5985f97e9897bd807da0ca7a98a3
ValenGal-prog <lesliegaleano637@gmail.com>
2024-07-14 21:17:12 -0500

    Update ChallengeLiteraturaApplicationTests.java

--- ChallengeLiteratura/ChallengeLiteratura/src/test/java/com/alura/ChallengeLiteratura/ChallengeLiteraturaApplicationTests.java
+++ ChallengeLiteratura/ChallengeLiteratura/src/test/java/com/alura/ChallengeLiteratura/ChallengeLiteraturaApplicationTests.java
@@ -2,12 +2,31 @@ package com.alura.ChallengeLiteratura;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import com.marcelobaranowski.LiterAluraMB.principal.Principal;
+import com.marcelobaranowski.LiterAluraMB.repository.AutorRepository;
+import com.marcelobaranowski.LiterAluraMB.repository.LibroRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootTest
 class ChallengeLiteraturaApplicationTests {
 
-	@Test
-	void contextLoads() {
+	@Autowired
+	private AutorRepository autorRepository;
+
+	@Autowired
+	private LibroRepository libroRepository;
+
+	public static void main(String[] args) {
+		SpringApplication.run(LiterAluraMbApplication.class, args);
 	}
+
+	@Override
+	public void run(String... args) throws Exception {
+
+		Principal principal = new Principal(libroRepository, autorRepository);
+		principal.mostrarMenuOpciones();
 
 }
